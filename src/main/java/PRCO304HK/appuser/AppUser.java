2b74bc2e9641336ee827f80d5fc17062dd773161
Andy Lam <lamchunandy@gmail.com>
2021-04-07 23:58:29 +0800

    07/04/2021 Bug fixes & Email verification implementation

--- src/main/java/PRCO304HK/appuser/AppUser.java
+++ src/main/java/PRCO304HK/appuser/AppUser.java
@@ -30,8 +30,8 @@ public class AppUser implements UserDetails {
             generator = "user_sequence"
     )
     private Long id;
-    private String name;
-    private String username;
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
@@ -39,20 +39,18 @@ public class AppUser implements UserDetails {
     private Boolean locked;
     private Boolean enabled;
 
-    public AppUser(String name,
-                   String username,
+    public AppUser(String firstName,
+                   String lastName,
                    String email,
                    String password,
-                   AppUserRole appUserRole,
-                   Boolean locked,
-                   Boolean enabled) {
-        this.name = name;
-        this.username = username;
+                   AppUserRole appUserRole) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.appUserRole = appUserRole;
-        this.locked = locked;
-        this.enabled = enabled;
+        this.locked = locked = false;
+        this.enabled = enabled = false;
     }
 
     @java.lang.Override
@@ -66,9 +64,17 @@ public class AppUser implements UserDetails {
         return password;
     }
 
-    @java.lang.Override
-    public java.lang.String getUsername() {
-        return username;
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    public java.lang.String getFirstName() {
+        return firstName;
+    }
+
+    public java.lang.String getLastName() {
+        return lastName;
     }
 
     @java.lang.Override
--- src/main/java/PRCO304HK/appuser/AppUserService.java
+++ src/main/java/PRCO304HK/appuser/AppUserService.java
@@ -1,11 +1,17 @@
 package PRCO304HK.appuser;
 
 
+import PRCO304HK.registration.token.ConfirmationToken;
+import PRCO304HK.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -13,11 +19,41 @@ public class AppUserService implements UserDetailsService {
 
     private final static String USER_NOT_FOUND_MSG =
             "user with email %s not found";
+
     private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return appUserRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+    }
+
+    public String signUpUser(AppUser appUser) {
+        boolean userExist = appUserRepository.findByEmail(appUser.getEmail())
+                .isPresent();
+
+        if(userExist) {
+            throw new IllegalStateException("Email Already taken");
+        }
+
+        String encodePassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+
+        appUser.setPassword(encodePassword);
+
+        appUserRepository.save(appUser);
+
+        String token = UUID.randomUUID().toString();
+        // TODO: Send confirmation token
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                appUser
+        );
+
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+        return token;
     }
 }
