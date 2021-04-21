cb47cb7ddb6ef3c11205a74d77d0b1652c711183
Andy Lam <lamchunandy@gmail.com>
2021-04-21 23:30:29 +0800

    21/04/2021 Enable Gmail EmailService to send verify code + Bug fixes

--- src/main/java/PRCO304HK/ANDYLAM/AndylamApplication.java
+++ src/main/java/PRCO304HK/ANDYLAM/AndylamApplication.java
@@ -2,12 +2,20 @@ package PRCO304HK.ANDYLAM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AndylamApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AndylamApplication.class, args);
 	}
 
+	@GetMapping
+	public String hello() {
+		return "Hello World";
+	}
 }
