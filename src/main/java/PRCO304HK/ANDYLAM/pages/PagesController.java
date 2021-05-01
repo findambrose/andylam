babd70d427c4cf68aa9b53de6abcdd7378c66b2f
findambrose <ambkyusya@gmail.com>
2021-05-01 22:32:27 +0300

    add contests, contestantsg and voting/ results

--- src/main/java/PRCO304HK/ANDYLAM/pages/PagesController.java
+++ src/main/java/PRCO304HK/ANDYLAM/pages/PagesController.java
@@ -2,15 +2,22 @@ package PRCO304HK.ANDYLAM.pages;
 
 
 import PRCO304HK.ANDYLAM.appuser.AppUser;
+import PRCO304HK.ANDYLAM.appuser.AppUserRepository;
+import PRCO304HK.ANDYLAM.appuser.AppUserService;
 import PRCO304HK.ANDYLAM.registration.RegistrationRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.security.Principal;
+
 @Controller
 public class PagesController {
 
+    @Autowired
+    AppUserRepository appUserRepository;
 
     @GetMapping("admin/create-account")
     public String showAdminRegistrationPage(Model model){
@@ -37,6 +44,13 @@ public class PagesController {
     public String login(Model model){
         model.addAttribute("regRequest", new RegistrationRequest());
         return "login";
+    }
+
+    @GetMapping("/profile")
+    public String getProfile(Model model, Principal principal){
+        AppUser user = appUserRepository.findByEmail(principal.getName()).get();
+        model.addAttribute("user", user);
+        return "profile";
     }
 
 }
